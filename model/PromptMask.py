c0a9ee105d749496572b743b8bf3688ca29ecb26
Bill845514379 <51468896+Bill845514379@users.noreply.github.com>
2021-12-29 16:17:37 +0800

    Update PromptMask.py

--- model/PromptMask.py
+++ model/PromptMask.py
@@ -15,11 +15,20 @@ device = torch.device(cfg['device'])
 class LMHead(nn.Module):
     def __init__(self):
         super(LMHead, self).__init__()
+        self.label_emb = nn.Embedding(cfg['word_size'], hyper_roberta['label_dim'])
+
         self.dence = nn.Linear(hyper_roberta['label_dim'], hyper_roberta['label_dim'])
         self.layer_norm = BertLayerNorm(hyper_roberta['label_dim'], eps=1e-5)
         self.classifer = nn.Linear(hyper_roberta['label_dim'], 2)
 
     def forward(self, input_x):
+
+        gumbel_softmax = F.gumbel_softmax(input_x, hard=True)
+        # x = torch.softmax(x, dim=1)
+        roberta_emb = self.label_emb.weight
+
+        input_x = torch.matmul(gumbel_softmax, roberta_emb)
+
         x = self.dence(input_x)
         x = gelu(x)
         x = self.layer_norm(x)
@@ -31,8 +40,6 @@ class PromptMask(nn.Module):
     def __init__(self):
         super(PromptMask, self).__init__()
         self.roberta = RobertaForMaskedLM.from_pretrained(path['roberta_path'])
-        self.label_emb = nn.Embedding(cfg['word_size'], hyper_roberta['label_dim'])
-        self.lm_head = LMHead()
 
     def forward(self, input_x):
         mask0 = (input_x == 50264)
@@ -42,11 +49,7 @@ class PromptMask(nn.Module):
         x = input_x[0]
         # x = self.lm_head(x)
         x = x[mask0]
-        gumbel_softmax = F.gumbel_softmax(x, hard=True)
-        # x = torch.softmax(x, dim=1)
-        roberta_emb = self.label_emb.weight
 
-        x = torch.matmul(gumbel_softmax, roberta_emb)
         x = self.lm_head(x)
 
         return x
